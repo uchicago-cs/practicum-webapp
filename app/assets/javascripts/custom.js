5e751f27a21aac1bd007ccd8177ea093777dc944
smlance <stefan@lances.net>
2014-08-15 20:23:37 -0500

    Give quarter form fields inline class
    - Also, give datetimepickers white backgrounds
    
    - Also, fix small bug where "SS" showed in second digits of quarter
      form fields

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.dropdown-toggle').dropdown()
     $('.datetimepicker').datetimepicker({
-	format: 'yyyy-MM-dd HH:mm:SS',
+	format: 'yyyy-MM-dd HH:mm:00',
 	// format: 'dd/MM/yyyy HH:mm PP',
 	pickSeconds: false,
 	language: 'en'
