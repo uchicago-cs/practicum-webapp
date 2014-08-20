6b4ebc23f48e6b5438b22448e706bcb3fa03167b
smlance <stefan@lances.net>
2014-08-20 11:26:44 -0500

    Add datetimepicker for bs3 and style quarter forms

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     $('.dropdown-toggle').dropdown()
     $('.datetimepicker').datetimepicker({
-	format: 'yyyy-MM-dd HH:mm:00',
-	// format: 'dd/MM/yyyy HH:mm PP',
-	pickSeconds: false,
+	format: 'YYYY-MM-DD HH:mm:00',
+	useSeconds: false,
+	useCurrent: false,
+	minuteStepping: 15,
 	language: 'en'
     });
 })
