223315bdc3dfe403d1b23f7693c17bc77a4462e5
smlance <stefan@lances.net>
2014-09-04 17:37:48 -0500

    Make LDAP RSpec tests work
    - See https://github.com/plataformatec/devise/wiki/
      How-To:-Test-with-Capybara
    
    - Also, capitalize all page titles

--- spec/features/ldap_spec.rb
+++ spec/features/ldap_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 require 'spec_helper'
 
 feature "\"Signing in\" with LDAP" do
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   before(:each) do
     @student = FactoryGirl.create(:student)
 
-    sign_in(@student, :no_capybara)
+    login_as(@student, scope: :user)
   end
 
-  describe "navigate to the home page" do
+  after(:each) { Warden.test_reset! }
+
+  describe "navigating to the home page" do
     it "should show my CNetID" do
       visit root_url
       expect(page).to have_content("#{@student.cnet}")
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,14 +1,6 @@
-def sign_in(user, options={})
-  # Takes a user and an optional options hash.
-  if options[:no_capybara]
-    # Sign in when not using Capybara.
-    remember_token = User.new_remember_token
-    cookies[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.digest(remember_token))
-  else
-    visit new_user_session_path
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Sign in"
-  end
-end
+# def sign_in(user)
+#   visit new_user_session_path
+#   fill_in "CNetID", with: user.cnet
+#   fill_in "Password", with: user.password
+#   click_button "Sign in"
+# end
