5f155689662af9048078650813e88f926731719d
smlance <stefan@lances.net>
2014-09-07 10:20:18 -0500

    Add word-wrapping to sub and proj tables

--- spec/features/view_project_spec.rb
+++ spec/features/view_project_spec.rb
@@ -34,9 +34,9 @@ describe "Viewing a project", type: :feature do
 
     describe "after the advisor has made a project" do
       before do
+        @project = FactoryGirl.create(:project, advisor: @advisor)
         ldap_sign_in(@advisor)
         visit root_url
-        @project = FactoryGirl.create(:project, advisor: @advisor)
       end
 
       # Note: these specs should test for the presence of "pending"
@@ -45,14 +45,13 @@ describe "Viewing a project", type: :feature do
       # and the project's name.
 
       it "should have a 'pending' and unpublished status" do
-        expect(project.status).to eq "pending"
-        expect(project.status_published).to eq false
+        expect(@project.status).to eq "pending"
+        expect(@project.status_published).to eq false
       end
 
       it "should show 'pending' to the advisor" do
-        click_link("My projects")
-        puts page.body * 2
-        click_link(project.name)
+        within("#dropdown-personal") { click_link("My projects") }
+        click_link(@project.name)
         within("table") do
           expect(page).to have_content("Pending")
         end
@@ -60,42 +59,45 @@ describe "Viewing a project", type: :feature do
 
       it "should not be in the published projects list" do
         click_link("Projects")
-        expect(page.text).not_to have_content(project.name)
+        expect(page.text).not_to have_content(@project.name)
       end
     end
 
-      # describe "an admin viewing the project" do
-      #   before do
-      #     click_link("Sign out")
-      #     ldap_sign_in(@admin)
-      #     visit root_url
-      #     click_link("Pending projects")
-      #   end
+    context "an admin viewing the project" do
 
-      #   it "should show 'pending'" do
-      #     within("table") do
-      #       expect(page).to have_content("Pending")
-      #     end
-      #   end
+      it "should show 'pending'" do
+        @project = FactoryGirl.create(:project, advisor: @advisor)
+        ldap_sign_in(@admin)
+        visit root_path
+        within("#dropdown-administrative") { click_link("Pending projects") }
+        within("table") do
+          expect(page).to have_content("Pending")
+        end
+      end
 
-      #   describe "an admin changing its status to 'accepted'", js: true do
-      #     before do
-      #       click_link(project.name)
-      #       choose "Approve"
-      #       click_button "Update project status"
-      #       page.evaluate_script('window.confirm = function() { return true; }')
-      #     end
+      describe "an admin changing its status to 'accepted'", js: true do
+        before do
+          @project = FactoryGirl.create(:project, advisor: @advisor)
+          ldap_sign_in(@admin)
+          visit root_path
+          visit pending_projects_path
+          click_link(@project.name)
+          choose "Approve"
+          click_button "Update project status"
+          page.evaluate_script('window.confirm = function() { return true; }')
+        end
 
-      #     it "should have changed its status to 'accepted'" do
-      #       expect(project.status).to eq "accepted"
-      #     end
+        it "should have changed its status to 'accepted'" do
+          puts @project.inspect * 50
+          expect(@project.status).to eq "accepted"
+        end
 
-      #     it "should show 'accepted / pending' message on its page" do
-      #       expect(page).to have_selector('div.alert.alert-notice')
-      #       within("table") do
-      #         expect(page).to have_content("Accepted (flagged")
-      #       end
-      #     end
+        # it "should show 'accepted / pending' message on its page" do
+        #   expect(page).to have_selector('div.alert.alert-notice')
+        #   within("table") do
+        #     expect(page).to have_content("Accepted (flagged")
+        #   end
+        # end
 
       #     it "should show 'accepted / pending' on pendng projects page" do
       #       before do
@@ -105,8 +107,8 @@ describe "Viewing a project", type: :feature do
       #         expect(page).to have_content("Accepted (flagged")
       #       end
       #     end
-      #  end
-      # end
+        end
+       end
 
     # end
 
