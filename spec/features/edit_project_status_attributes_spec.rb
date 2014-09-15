1eb5b52156357c8fdc05c3571f1759b25689cdd9
smlance <stefan@lances.net>
2014-09-15 08:58:33 -0500

    Set up edit_project_status specs

--- spec/features/edit_project_status_attributes_spec.rb
+++ spec/features/edit_project_status_attributes_spec.rb
@@ -77,14 +77,87 @@ describe "Editing a submission's 'status' attributes", type: :feature do
 
         context "viewed by the admin" do
 
+          context "visiting the advisor's my_projects page" do
+            before(:each) { visit users_projects_path(@advisor) }
+
+            it "should show the updated status" do
+              within("table") do
+                expect(page).to have_content("Accepted (flagged, not " +
+                                             "published")
+                expect(page).not_to have_content("Pending")
+              end
+            end
+
+          end
+
+          context "visiting the projects index page" do
+            before(:each) { visit projects_path }
+
+            it "should not show the project" do
+              expect(page).not_to have_content(@project.name)
+            end
+
+          end
+
+          context "visiting the project's page" do
+            before(:each) { visit project_path(@project) }
+
+            it "should show the updated status" do
+              within('tr', text: "Status") do
+                expect(page.find("#project_status_accepted")).to be_checked
+                expect(page.find("#project_status_published")).
+                  not_to be_checked
+              end
+            end
+
+          end
+
         end
 
         context "viewed by the advisor" do
 
+          before(:each) do
+            logout
+            ldap_sign_in(@advisor)
+          end
+
+          context "visiting the advisor's my_projects page" do
+
+          end
+
+          context "visiting the projects index page" do
+
+          end
+
+          context "visiting the project's page" do
+
+          end
+          # Visiting the my_projects page
+          # Visiting the projects index page
+          # Visiting the project's page
         end
 
         context "viewed by the student" do
 
+          before(:each) do
+            logout
+            ldap_sign_in(@student)
+          end
+
+          context "visiting the advisor's my_projects page" do
+
+          end
+
+          context "visiting the projects index page" do
+
+          end
+
+          context "visiting the project's page" do
+
+          end
+          # Visiting the advisor's my_projects page
+          # Visiting the projects index page
+          # Visiting the project's page
         end
 
       end
@@ -96,13 +169,85 @@ describe "Editing a submission's 'status' attributes", type: :feature do
 
   context "publishing the decision (accepted)" do
 
+        context "visiting the project page" do
+      before(:each) { visit project_path(@project) }
+
+      context "updating the project's status" do
+        before(:each) do
+          choose "Approve"
+          click_button "Update project status"
+        end
+
+        it "should change the project's status" do
+          expect(@project.reload.status).to eq("accepted")
+        end
+
+        context "viewed by the admin" do
+          # Visiting the advisor's my_projects page
+          # Visiting the projects index page
+          # Visiting the project's page
+        end
+
+        context "viewed by the advisor" do
+          # Visiting the my_projects page
+          # Visiting the projects index page
+          # Visiting the project's page
+        end
+
+        context "viewed by the student" do
+          # Visiting the advisor's my_projects page
+          # Visiting the projects index page
+          # Visiting the project's page
+        end
+
+      end
+
+    end
+
   end
 
   context "publishing the decision (rejected)" do
 
+        context "visiting the project page" do
+      before(:each) { visit project_path(@project) }
+
+      context "updating the project's status" do
+        before(:each) do
+          choose "Approve"
+          click_button "Update project status"
+        end
+
+        it "should change the project's status" do
+          expect(@project.reload.status).to eq("accepted")
+        end
+
+        context "viewed by the admin" do
+          # Visiting the advisor's my_projects page
+          # Visiting the projects index page
+          # Visiting the project's page
+        end
+
+        context "viewed by the advisor" do
+          # Visiting the my_projects page
+          # Visiting the projects index page
+          # Visiting the project's page
+        end
+
+        context "viewed by the student" do
+          # Visiting the advisor's my_projects page
+          # Visiting the projects index page
+          # Visiting the project's page
+        end
+
+      end
+
+    end
+
   end
 
   context "editing the proposal" do
+
+
 
   end
 
