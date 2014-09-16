82afdd239254b57d2d56db3ca9bebe335797829a
smlance <stefan@lances.net>
2014-09-16 10:16:21 -0500

    Finish request_advisor_status specs

--- spec/features/request_advisor_status_spec.rb
+++ spec/features/request_advisor_status_spec.rb
@@ -12,6 +12,7 @@ describe "Requesting advisor status", type: :feature do
     @quarter = FactoryGirl.create(:quarter, :no_deadlines_passed)
     @user    = FactoryGirl.create(:user)
     @advisor = FactoryGirl.create(:advisor)
+    @admin   = FactoryGirl.create(:admin)
   end
 
   context "as a non-advisor" do
@@ -57,29 +58,56 @@ describe "Requesting advisor status", type: :feature do
           expect(current_path).to eq(root_path)
         end
 
+        it "should show the temporary 'your request has been sent' message" do
+          click_link "Submit request"
+          expect(page).to have_selector("div.alert.alert-success")
+          expect(page).to have_content("You have requested advisor privileges")
+        end
+
         it "should show the advisor_status_pending message on every page" do
           click_link "Submit request"
-          save_and_open_page
           expect(page).to have_selector("div.alert.alert-info")
-          expect(page).to have_content("pending")
+          expect(page).to have_content("pending approval by an administrator")
           # Visit an arbitrary page to test for the presence of the message.
           visit projects_path
           expect(page).to have_selector("div.alert.alert-info")
-          expect(page).to have_content("pending")
+          expect(page).to have_content("pending approval by an administrator")
+          # It won't have the temporary message, though.
+          expect(page).not_to have_selector("div.alert.alert-success")
         end
 
       end
 
       context "when their request is approved" do
 
-        # Do this as an admin.
-        # Then, sign in as the student.
-
-        it "should make the user an advisor" do
-
+        before(:each) do
+          logout
+          ldap_sign_in(@admin)
+          visit user_path(@user)
+          check "Advisor"
+          click_button "Change this user's roles"
         end
 
-        it "should remove the message that appeared on every page" do
+        it "should make the user an advisor" do
+          expect(@user.reload.advisor).to eq(true)
+        end
+
+        it "should turn off the user's 'advisor status pending' status" do
+          expect(@user.reload.advisor_status_pending).to eq(false)
+        end
+
+        context "and they are signed in" do
+
+          before(:each) do
+            logout
+            ldap_sign_in(@user)
+          end
+
+          it "should remove the message that appeared on every page" do
+            visit root_url
+            expect(page).not_to have_selector("div.alert.alert-info")
+            expect(page).not_to have_content("pending approval by an admin")
+          end
 
         end
 
@@ -94,7 +122,10 @@ describe "Requesting advisor status", type: :feature do
     before(:each) { ldap_sign_in(@advisor) }
 
     it "should redirect to the homepage" do
-
+      visit request_advisor_access_path
+      expect(current_path).to eq(root_path)
+      expect(page).to have_selector("div.alert.alert-danger")
+      expect(page).to have_content("You are already an advisor")
     end
 
   end
