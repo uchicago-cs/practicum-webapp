d5c42e567838b134434c5ebd769452fdd06ffe9a
smlance <stefan@lances.net>
2014-09-23 15:32:33 -0500

    Copy methods from evals cont. to templates cont.
    - Copy some methods from evaluations controller to evaluation_templates
      controller

--- app/controllers/evaluation_templates_controller.rb
+++ app/controllers/evaluation_templates_controller.rb
@@ -1,6 +1,11 @@
 class EvaluationTemplatesController < ApplicationController
   load_and_authorize_resource
 
+  before_action :ensure_unique_question_positions, only: :update
+
+  # Ideally, use strong parameters throughout (just use template_params instead
+  # of params).
+
   def index
   end
 
@@ -17,19 +22,54 @@ class EvaluationTemplatesController < ApplicationController
   end
 
   def update
+    @evaluation_template.update_survey(params)
+    if @evaluation_template.save
+      flash[:success] = "Template updated."
+      redirect_to @evaluation_template
+    else
+      flash.now[:error] = "Template could not be updated."
+      render 'edit'
+    end
   end
 
   def destroy
   end
 
   def add_question
+    @evaluation_template.add_question(params)
+
+    if @evaluation_template.save
+      flash[:success] = "Question added to template."
+      # Fix routes.rb re: this.
+      redirect_to eit_evaluation_template_path(@evaluation_template)
+    else
+      flash[:error] = "Question was unable to be added."
+      render 'edit'
+    end
   end
 
   def update_question
+    @evaluation_template.edit_question(params)
+
+    if @evaluation_template.save
+      flash[:success] = "Question updated."
+      # Fix routes.rb re: this.
+      redirect_to edit_evaluation_template_path(@evaluation_template)
+    else
+      flash.now[:error] = "Question could not be updated."
+      render 'edit'
+    end
   end
 
   private
 
+  def template_params
+
+  end
+
   def ensure_unique_question_positions
+    message = "No two questions can have the same position."
+    redirect_to edit_evaluation_template_path, flash: { error: message } if
+      params[:ordering].values.length != params[:ordering].values.uniq.length
   end
 end
--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -6,50 +6,10 @@ class EvaluationsController < ApplicationController
   before_action :already_evaluated?,            only: [:new, :create]
   before_action :is_admin?,                     only: :index
   before_action :submission_status_sufficient?, only: [:new, :create]
-  before_action :prevent_dup_positions,         only: :update_template
   before_action :get_template
   before_action :get_student,                   only: [:new, :create]
 
   def index
-  end
-
-  def edit_template
-  end
-
-  def update_template
-    @template.update_survey(params)
-
-    if @template.save
-      flash[:success] = "Template updated."
-      redirect_to edit_evaluation_template_path
-    else
-      flash.now[:error] = "Template could not be updated."
-      render 'edit_template'
-    end
-  end
-
-  def update_template_question
-    @template.edit_question(params)
-
-    if @template.save
-      flash[:success] = "Question updated."
-      redirect_to edit_evaluation_template_path
-    else
-      flash.now[:error] = "Question could not be updated."
-      render 'edit_template'
-    end
-  end
-
-  def add_to_template
-    @template.add_question(params)
-
-    if @template.save
-      flash[:success] = "Question added to template."
-      redirect_to edit_evaluation_template_path
-    else
-      flash[:error] = "Question was unable to be added."
-      render 'edit_template'
-    end
   end
 
   def show
@@ -97,12 +57,6 @@ class EvaluationsController < ApplicationController
 
   def get_template
     @template = EvaluationTemplate.first || EvaluationTemplate.new
-  end
-
-  def prevent_dup_positions
-    message = "No two questions can have the same position."
-    redirect_to edit_evaluation_template_path, flash: { error: message } if
-      params[:ordering].values.length != params[:ordering].values.uniq.length
   end
 
   def get_student
