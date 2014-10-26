ad85f64b7bd04f8259f45e3640e2a191198c8e4c
smlance <stefan@lances.net>
2014-10-25 23:39:00 -0500

    Fix routes for LdapUser and LocalUser

--- app/models/ldap_user.rb
+++ app/models/ldap_user.rb
@@ -21,4 +21,11 @@ class LdapUser < User
     end
   end
 
+  # Fix routes for STI subclass (LocalUser) of User so that we can call
+  # current_user and generate a path in the view, rather than calling
+  # user_path(current_user).
+  def self.model_name
+    User.model_name
+  end
+
 end
--- app/models/local_user.rb
+++ app/models/local_user.rb
@@ -14,5 +14,11 @@ class LocalUser < User
   validates :password, confirmation: true, presence: true,
                        length: { minimum: 8 }
 
+  # Fix routes for STI subclass (LocalUser) of User so that we can call
+  # current_user and generate a path in the view, rather than calling
+  # user_path(current_user).
+  def self.model_name
+    User.model_name
+  end
 
 end
