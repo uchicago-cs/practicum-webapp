100bd5a844983d03dbb2fb3593c3791c79a15f36
smlance <stefan@lances.net>
2014-10-25 23:06:19 -0500

    Clean up signup page and validation messages

--- app/models/local_user.rb
+++ app/models/local_user.rb
@@ -1,12 +1,18 @@
 class LocalUser < User
 
-  devise :trackable, :validatable, :database_authenticatable, :registerable,
+  devise :trackable, :database_authenticatable, :registerable,
          :recoverable
+
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   # Password confirmation is needed only on registration, i.e., for
   # local_users only.
   attr_accessor :password_confirmation
-  validates :password, confirmation: true
   validates :password_confirmation, presence: true
+  validates :password, confirmation: true, presence: true,
+                       length: { minimum: 8 }
+
 
 end
