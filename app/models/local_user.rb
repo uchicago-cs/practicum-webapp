56f5a780bf563433fe9f808ae421ae3b5d4d3677
smlance <stefan@lances.net>
2014-11-09 14:37:28 -0600

    Update gems; fix bug re: admin approving new users
    - Allow admins to approve new local users

--- app/models/local_user.rb
+++ app/models/local_user.rb
@@ -3,18 +3,18 @@ class LocalUser < User
   devise :trackable, :database_authenticatable, :registerable,
          :recoverable
 
-  validates :email, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :affiliation, presence: true
-  validates :department, presence: true
+  validates :email, presence: true, on: :create
+  validates :first_name, presence: true, on: :create
+  validates :last_name, presence: true, on: :create
+  validates :affiliation, presence: true, on: :create
+  validates :department, presence: true, on: :create
 
   # Password confirmation is needed only on registration, i.e., for
   # local_users only.
   attr_accessor :password_confirmation
-  validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true, on: :create
   validates :password, confirmation: true, presence: true,
-                       length: { minimum: 8 }
+                       length: { minimum: 8 }, on: :create
 
   after_create :send_admin_email
 
