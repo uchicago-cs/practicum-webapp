da97224a6fa42b741945b9625f55e1fac7da2fed
smlance <stefan@lances.net>
2014-11-09 16:47:59 -0600

    Make specs work with user model STI

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -3,5 +3,5 @@ include Warden::Test::Helpers
 
 # Make the user signed in without signing them in (i.e., bypass LDAP auth).
 def ldap_sign_in(user)
-  login_as(user, scope: :user)
+  login_as(user, scope: :ldap_user)
 end
